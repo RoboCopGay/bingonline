d7d8585ae036ad3581c77fc91b404a114d09dda7
RoboCopGay <vinicios.sousa909@gmail.com>
2020-02-16 14:49:14 -0300

    preparing date register test

---
+++ server/test.adding_data.py
@@ -0,0 +1,79 @@
+from models import *
+
+users = [
+    {
+        'name': 'joao',
+        'username': 'joao',
+        'email': 'joao@j.com',
+        'password': 'joao123'
+    },
+    {
+        'name': 'maria',
+        'username': 'maria',
+        'email': 'maria@j.com',
+        'password': 'maria123'
+    },
+    {
+        'name': 'joana',
+        'username': 'joana',
+        'email': 'joana@g.com',
+        'password': 'morteaoshomens123'
+    },
+    {
+        'name': 'joao roberto',
+        'username': 'joaobeto',
+        'email': 'joaobeto@docao.com',
+        'password': 'joao123'
+    },
+    {
+        'name': 'gabriel',
+        'username': 'gabi',
+        'email': 'gabi@j.com',
+        'password': 'g4b1234'
+    },
+    {
+        'name': 'ramires',
+        'username': 'golias',
+        'email': 'golias@boi.com',
+        'password': 'opirocudo'
+    },
+]
+
+addresses = [
+    {
+        'cep': '00000000',
+        'city': 'zerada',
+        'state': 'zero',
+        'number': '00',
+        'street': 'joao',
+        'country': 'do cão',
+        'neighborhood': 'inferno',
+    },
+    {
+        'cep': '99998888',
+        'city': 'belo boga',
+        'state': 'fim de mundo',
+        'number': '08',
+        'street': 'maria',
+        'country': 'do cão',
+        'neighborhood': 'lula molusco',
+    },
+    {
+        'cep': '99998888',
+        'city': 'belo boga',
+        'state': 'fim de mundo',
+        'number': '02',
+        'street': 'maria',
+        'country': 'do cão',
+        'neighborhood': 'lula molusco',
+    },
+    {
+        'cep': '99444488',
+        'city': 'belo boga',
+        'state': 'fim de mundo',
+        'number': '75',
+        'street': 'joana',
+        'country': 'do cão',
+        'neighborhood': 'fi do capeta',
+    },
+]
