7d26ed80c57d50005a2a16efb8e9e3f8ffed7684
RoboCopGay <vinicios.sousa909@gmail.com>
2020-02-21 20:09:49 -0300

    clearing the code

--- server/app.py
+++ server/app.py
@@ -1,15 +1,13 @@
 from flask import Flask
-try:
-    from flask.ext.sqlalchemy import SQLAlchemy
-except ImportError:
-    from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
 
 db = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
+
 if __name__ == "__main__":
     from views import *
-
     app.run(debug=True)
