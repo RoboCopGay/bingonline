fd99067482be7ff08d29dafa26c10a03fc7e6bff
RoboCopGay <vinicios.sousa909@gmail.com>
2020-02-22 13:28:15 -0300

    fixing bug on showing of database data

--- server/test.list_tables.py
+++ server/test.list_tables.py
@@ -1,5 +1,9 @@
 from models import *
+from os import system as e
+e('clear')
 
 for i in [User, Board, Prize, Event]:
-    print(i.__tablename__+':', ', '.join(i.query.all()))
-    print('\n')
+    print(i.__tablename__+':', end=' ')
+    i = i.query.all()
+    print(', '.join([ str(j) for j in i]))
+    print()
