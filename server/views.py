4efee8706d97be0a11d23c94f3983d25d354a14a
RoboCopGay <vinicios.sousa909@gmail.com>
2020-02-29 22:59:52 -0300

    initializated the event manager

--- server/views.py
+++ server/views.py
@@ -69,3 +69,25 @@ def get_user(username):
             'type': 'error',
             'data': 'user not found!!'
         }), 404
+
+@app.route('/user/<username>/event/', methods=['POST', 'GET'])
+def event(username):
+    user = User.query.filter_by(username=username).first()
+    if request.method == 'GET':
+        if user:
+            return jsonify({
+                'type': 'sucess',
+                'data': [ dict(
+                    name=e.name,
+                    description=e.description,
+                    date=str(e.date),
+                    fisic_ball=e.fisic_ball
+                ) for e in Event.query.filter_by(owner=user).all()]
+            })
+        else:
+            return jsonify({
+                'type': 'error',
+                'data': 'user not found!!'
+            }), 404
+    elif request.method == 'POST':
+        pass
