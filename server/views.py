88cd33b9f02184d0bd8691f0af86db69b8afd8a6
RoboCopGay <vinicios.sousa909@gmail.com>
2020-03-01 00:14:07 -0300

    adding the login route and verifying the logged user for event creation

--- server/views.py
+++ server/views.py
@@ -1,4 +1,4 @@
-from flask import request, jsonify, redirect, abort
+from flask import request, jsonify, redirect, make_response
 from mailutil import *
 
 from models import *
@@ -13,18 +13,57 @@ def index():
 
 @app.route('/user/', methods = ['POST'])
 def user():
-    email = request.json['email'].strip()
-    if check_email_validation(email):
-        send_confirmation_mail(request.json)
-    else:
+    data = request.json['data']
+    if request.json['type'] == 'create':
+        email = data['email'].strip()
+        if check_email_validation(email):
+            send_confirmation_mail(request.json)
+        else:
+            return jsonify({
+                'type': 'error',
+                'data': 'email is not valid!'
+            }), 500
         return jsonify({
-            'type': 'error',
-            'data': 'email is not valid!'
-        }), 500
-    return jsonify({
-        'type':  'sucess',
-        'data':  request.json
-    })
+            'type':  'sucess',
+            'data':  data
+        })
+
+    elif request.json['type'] == 'login':
+        response = make_response(jsonify({
+            'type': 'sucess',
+            'data': 'login realized!'
+        }))
+
+        if request.cookies.get('bingonline.logged'):
+            return response
+
+        if 'password' in data:
+
+            user = None
+            if 'username' in data:
+                user = User.query.filter_by(username=data['username']).first()
+            elif 'email' in data:
+                user = User.query.filter_by(email=data['email']).first()
+            else:
+                return jsonify({
+                    'type': 'error',
+                    'data': 'No user credentials (username or email) on request'
+                }), 500
+
+            if user:
+                if user.check_pass(data['password']):
+                    return response
+
+            else:
+                return jsonify({
+                    'type': 'error',
+                    'data': 'user not found!!'
+                }), 404
+        else:
+            return jsonify({
+                'type': 'error',
+                'data': 'No user credentials (password) on request'
+            }), 500
 
 @app.route('/user/confirm_email/<token>/')
 def confirm_email(token):
@@ -90,4 +129,7 @@ def event(username):
                 'data': 'user not found!!'
             }), 404
     elif request.method == 'POST':
-        pass
+        print(request.cookies)
+        log = request.cookies.get('bingonline.logged')
+        log_user = request.cookies.get('bingonline.username')
+        return 'ok!'
