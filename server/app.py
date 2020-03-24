9317228071320f52ded9725ce9647595501c6a61
RoboCopGay <vinicios.sousa909@gmail.com>
2020-03-24 15:55:27 -0300

    add a secret_key

--- server/app.py
+++ server/app.py
@@ -5,6 +5,7 @@ from config import config
 app = Flask(__name__)
 
 db = SQLAlchemy(app)
+app.config['SECRET_KEY'] = config.SECRET_KEY
 app.config['SQLALCHEMY_DATABASE_URI'] = config.DB_PATH
 app.config['DB_PATH'] = config.DB_PATH
 
